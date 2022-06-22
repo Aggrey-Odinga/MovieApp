ab9a4ca0cd41ee7e23cfc55a4838c2d268c4ab44
Aggrey-Odinga <aggrey.odinga@student.moringaschool.com>
2022-06-22 08:55:14 +0300

    Add constants class to reference Api

---
+++ app/src/main/java/com/moringaschool/movieapp/Constants.java
@@ -0,0 +1,9 @@
+package com.moringaschool.movieapp;
+
+import com.moringaschool.movieapp.BuildConfig;
+
+public class Constants {
+    public static final String MovieApp_Base_Url = "https://api.themoviedb.org/3/";
+//    "https://api.themoviedb.org/3/"
+    public static final String API_KEY = BuildConfig.API_KEY;
+}
